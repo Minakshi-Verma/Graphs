9d1aa9ef0464801614b53ea82066371f6ad71192
Minakshi Verma <mvedwan@gmail.com>
2020-07-14 19:04:59 -0400

    Working on ancestor.py

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,32 @@
 
+from util import Queue
+
+
+
 def earliest_ancestor(ancestors, starting_node):
-    pass
+    # BFS
+    # parent and children make the nodes
+    # relationship between parent and child make the edge
+
+    # make a stack
+    q = Queue()
+    #add the node to the path( only one node in the starting)
+    path = [starting_node]
+
+    #add the path to the queue
+    q.enqueue(path)
+
+    # make a set to track if we've visited there
+    visited = set()
+
+    # while the queue isn't empty
+    while q.size():
+        ##pop off whatever's at the front of stack, this is our current node
+        current_node = q.dequeue()
+        ##if we haven't visisted this node yet
+        if current_node not in visited:
+            #mark as visisted
+            visited.add(current_node)
+
+            #get the children--neighbors
+
